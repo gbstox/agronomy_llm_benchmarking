2942d6e3b6063deb727b5f936f0aa7c6e3ba87b9
gbs <gbs>
2024-01-01 23:38:31 -0800

    Rearrage & finish benchmark runs

---
+++ README.md
@@ -0,0 +1,19 @@
+# Leaderboard
+Model Name | Score | Date |
+-----------|-------|------|
+gpt-4-1106-preview     | 85.86%  | 2023-01-01 |
+gpt-4     | 80.81%  | 2023-01-01 |
+fbn-norm     | 75.76%  | 2023-01-01 |
+gpt-3.5-turbo     | 69.7%  | 2023-01-01 |
+
+
+# What is this?
+We are benchmarking the ability for different models to give correct answers to Agronomy questions. 
+
+# Why?
+When building new models for agriculture, it's important to know if you are getting better or worse. This is a simple benchmark to acoomplish this.
+
+# Roadmap
+1. Add short and long answer questions (to be evaluated against example correct answers)
+2. Add questions for international regions
+3. Add more models to the leaderboard
--- main.py
+++ main.py
@@ -1,58 +1,117 @@
-import requests
+import os
+import json
+import datetime
+from pathlib import Path
+from dotenv import load_dotenv
+from openai import OpenAI
 
-def prompt_fbn_norm(session_id, prompt):
-    url = 'https://www.fbn.com/api/notifications/norm/chat/prompt'
+import private_utils
 
-    headers = {
-        'authority': 'www.fbn.com',
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/json',
-        'dnt': '1',
-        'origin': 'https://www.fbn.com',
-        'referer': 'https://www.fbn.com/norm',
-        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'x-csrf-token': '3d2fa262a0abae7bc4cafee703dc1c7ebf4a83536b5b17dbedafe2c31849d7f5',
-        'x-datadog-trace-id': '926791915438938916',
-        'x-js-build-tag': 'build-642bd252496f7104-3944001',
-        'x-requested-with': 'XMLHttpRequest',
-        # Add your cookies here
-        'cookie': '_ga=GA1.1.1066855511.1703907093; _mkto_trk=id:197-HFR-752&token:_mch-fbn.com-1703907093092-42493; _gcl_au=1.1.779175757.1703907093; ajs_anonymous_id=6f729cff-ba40-4b95-8652-334aceffceaf; _fbp=fb.1.1703907093912.176531731; landingPageURLRaw=aHR0cHM6Ly93d3cuZmJuLmNvbS9ub3Jt; landingPageURL=aHR0cHM6Ly93d3cuZmJuLmNvbS9ub3Jt; referrerPageURLRaw=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8=; referrerPageURL=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8=; formURLRaw=aHR0cHM6Ly93d3cuZmJuLmNvbS9zaWdudXA=; formURL=aHR0cHM6Ly93d3cuZmJuLmNvbS9zaWdudXA=; ajs_user_id=165467; user_logged_in=1; fbnAuth=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJjb29raWUiOnRydWUsImV4cCI6MTcxODQyMjQzMywianRpIjoiOGRkZDcxNWVhZTAyNjJjYzY5YmEwNDQ5NGI3ODBlYjlkZDE4MTE1MWQ1MjExNDZjMjAzNWIyMmI1MzJhMzIzMSIsImtleSI6ImU1ZDkyMzk4OTVmYjM1MDBjNzdkNmRkMjAxNWU3MmQzYTgzNGM2NTZmNzU0ZTZkNzgwZjNhZmVhZDVhOWI5ODQiLCJzdWIiOjE2NTQ2NywidHlwIjoibG9naW4iLCJ2ZXIiOjF9.giRUTmvRPNiAPDvV1mk3uNmZcYtn94-cEcUftAGZQFb7XT9EqwPnl4coJEpT0aL0Skqfozimy9Uhcqgsj2-WQbBDI_LkNDbxGL8cbhZDFvgVQfEH_H5iH6D0OPjvc38VTGs_1gx-23dO5wKpVAcehMwOyySkBjYwq-S84XhEUhlb_zf4oTMqx4y0qcBRSMvfVqyyxeeINv27fgznFWFkEO--ilO1NH979xvRrpauc4gTPjLnca26dc244TIfp7YerkD1uh5PCQinE80HseSG8gttxoVMec2rcdyYMASHGlHwX-PmAmfstGZQtsWSWLq0Gxi-kGHTdlicihcfBoUOqQ; _clck=i1rp5i%7C2%7Cfi0%7C0%7C1459; analytics_session_id=1704008492867; analytics_session_id.last_access=1704009340247; _uetsid=ed9cd400a6c311ee852bfb47b0eaf23e; _uetvid=ed9ce600a6c311eebcbead0030aa6b92; _ga_E36VZHGD7X=GS1.1.1704008492.3.1.1704009340.58.0.0; _clsk=9ib4w4%7C1704009340495%7C2%7C1%7Ca.clarity.ms%2Fcollect; last_activity_timestamp=1704009340795'  # truncated for brevity
-    }
 
-    data = {
-        'session_id': session_id,
-        #'postal_code': postal_code,
-        #'country_code': country_code,
-        'prompt': prompt
-    }
+load_dotenv() 
 
-    response = requests.post(url, headers=headers, json=data)
-    return response.json()
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+client = OpenAI()
 
-# Example usage
-#response = prompt_fbn_norm("8e9437f8-8d15-4cb9-7da1-b376b3fb0a8f", "hello")
-with open('quizlet.txt', 'r') as file:
-    lines = file.readlines()
 
-benchmark_test = {
-  "multiple_choice": [
-    {"id" : "",
-     "question": "",
-     "answer_options": [
-       {"1." : ""},
-       {"2." : ""},
-       {"3." : ""},
-       {"4." : ""}
-     ],
-     "correct_answer": ""
-    },
-  ]
-}
+
+def build_multiple_choice_prompt(benchmark_question):
+    prompt = f"""
+        For the following multiple choice question, answer with one of the anser_options.
+        Reply with ONLY the answer_options key of the correct answer and nothing else. [No Prose]
+        
+        question: 
+        {benchmark_question["question"]}
+
+        answer_options: 
+        {benchmark_question["answer_options"]}
+        
+        """
+    return prompt
+    
+
+def prompt_openai(model_name, prompt):
+    response = client.chat.completions.create(
+        model= model_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful and brilliant agronomist."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    response_text = response.choices[0].message.content
+    return response_text
+
+
+def run_benchmark(model_name, benchmark_questions_file, retries = 3):
+    with open(benchmark_questions_file, 'r') as f:
+        benchmark_questions = json.load(f)
+
+    model_answers = {"multiple_choice": []}  # Initialize model_answers dictionary
+    model_answers["date"] = datetime.datetime.now().isoformat()
+    model_answers["model_name"] = model_name
+
+    # handle multiple choice questions
+    for benchmark_mc_question in benchmark_questions["multiple_choice"]:
+        prompt = build_multiple_choice_prompt(benchmark_mc_question)
+        model_answer = "fail"  # default answer if retries fail
+        for _ in range(retries):  # retry n number of times
+            if model_name == "fbn-norm":
+                temp_answer = private_utils.prompt_fbn_norm(prompt)
+            else:
+                temp_answer = prompt_openai(model_name, prompt)
+            print (temp_answer, f" try #{_}")
+            if len(temp_answer) == 1:  # if the answer is a single character
+                model_answer = temp_answer  # update the model answer
+                break  # exit the loop
+
+        benchmark_mc_question["model_answer"] = model_answer
+        model_answers["multiple_choice"].append(benchmark_mc_question)  # Append the question to model_answers
+
+
+    Path('benchmark_results').mkdir(parents=True, exist_ok=True)
+    with open(f'benchmark_results/{model_name}_answers.json', 'w') as f:
+        f.write(json.dumps(model_answers, indent=4))
+
+def score_multile_choice_answers(model_answers_file):
+    with open(model_answers_file, 'r') as f:
+        model_answers = json.load(f)["multiple_choice"]
+
+    correct_answers = 0
+    total_questions = len(model_answers)
+
+    for model_answer in model_answers:
+        if model_answer["model_answer"] == model_answer["correct_answer"]:
+            correct_answers += 1
+
+    score = round((correct_answers / total_questions) * 100, 2)
+    return score
+
+
+
+def compare_benchmark_scores():
+    benchmark_results_files = os.listdir('./benchmark_results')
+    for model_answers_file in benchmark_results_files:
+        score = score_multile_choice_answers(os.path.join('./benchmark_results', model_answers_file))
+        print(f'The model {model_answers_file} scored {score}% on the benchmark.')
+
+
+
+#model_name = "gpt-4"
+#model_name = 'gpt-3.5-turbo'
+#model_name = 'gpt-4-1106-preview'
+#model_name = "fbn-norm"
+model_names = ["gpt-4", "gpt-3.5-turbo", "gpt-4-1106-preview", "fbn-norm"]
+
+benchmark_questions_file = "./agronomy_benchmark_questions.json"
+
+for model_name in model_names:
+    print ()
+    print (model_name)
+    print()
+    run_benchmark(model_name, benchmark_questions_file)
+
+compare_benchmark_scores()
+
+
+
 
